6b5a5d1c84d15ddf6c1c253e85af5faa48cba6a2
subhankar-das-phantom <subhankarnew1@gmail.com>
2025-09-01 13:34:34 +0530

    Update api.js

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,3 +1,3 @@
 // src/config/api.js
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export default API_BASE_URL;
