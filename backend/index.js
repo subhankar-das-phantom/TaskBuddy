328063d9da12359f489f09bf317aba533221c659
subhankar-das-phantom <subhankarnew1@gmail.com>
2025-09-01 15:11:14 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,8 +17,7 @@ const __dirname = path.dirname(__filename);
 app.use(cors());
 app.use(express.json());
 
-// Serve frontend
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
 
 
 // MongoDB connection
@@ -33,10 +32,7 @@ connection.once('open', () => {
 app.use('/api/users', usersRouter);
 app.use('/api/tasks', tasksRouter);
 
-// Catch-all route to serve frontend
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
-});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
